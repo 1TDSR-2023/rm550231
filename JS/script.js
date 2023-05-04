59cedb083169cef639b9e884cea40daaf97275ff
Pedro-Ferrari <pedrofcmp@gmail.com>
2023-05-03 23:12:17 -0300

    Funções Math, explicação DOM...

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,19 @@
-var nome1 = "Petroni";
-//let nome2 = "Derick";
-//const nome3 = "Ruan";
+let botao = document.getElementById("meu-btn");
+console.log(botao.textContent);
 
-if(nome1 != ""){
-    let nome1 = "Geovana";
+botao.addEventListener("click",function(){
+    //console.log(this.textContent)
+    //função matematica Math
+    //random: retorna numero aleatorio entre 0 e 1 Ex: 0.79756442457;
+    //floor: Ela arredonda um número para baixo    
+    //ceil: Ela arredinda um numero para cima
+    //round: arredonda aleatoriamente
+    let r=0,g=0,b=0;
+    r = Math.round(Math.random()*255);
+    g = Math.round(Math.random()*255);
+    b = Math.round(Math.random()*255);
+    
+    this.setAttribute("style",`backgroung-color:rgb(${r},${g},${b});`);
+ });
 
-}
-
-console.log(nome1);
+ //Estudar: querySelector , querySelectorAll , Array , Estruturas de repetição.
